c4d6e20cf1f405170443b42f0888a182d4df63d1
spunkyalexsolis <spunkyalexsolis@gmail.com>
2024-11-11 13:49:51 -0800

    Project Initialization

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { SampleChart } from "./components/sample-chart";
+
+export default function DashboardPage() {
+    return <div>
+        <h1>Dashboard</h1>
+        <SampleChart/>
+    </div>;
+}
